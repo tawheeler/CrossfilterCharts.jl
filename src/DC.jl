1ef2965635032c68857dc94fadf051a9efbed955
Tim Wheeler <wheelert@stanford.edu>
2016-05-12 20:54:15 -0700

    proof of concept

--- src/DC.jl
+++ src/DC.jl
@@ -1,3 +1,28 @@
-module DC
+# module DC
 
-end # module
+import Base.writemime
+
+type DCOut
+end
+
+function writemime(io::IO, ::MIME"text/html", dcout::DCOut)
+	
+
+	# If IJulia is present, go for it
+	if isdefined(Main, :IJulia)
+		#=
+		1 - determine chart layout
+		2 - generate iframe html + js page
+		3 - write link to it
+		=#
+
+		write(io, """<iframe src="src.htm" width="975" height="500"></iframe>""")
+	else
+		# TODO
+		# decide what to do in the absence of IJulia
+	end
+end
+
+# end # module
+
+# <link href="https://cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.css" rel="stylesheet">
