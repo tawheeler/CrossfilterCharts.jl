2082e4e7241b3d52d11eac078037f69d3dad1f47
Tim Wheeler <wheelert@stanford.edu>
2016-06-01 21:43:26 -0700

    updating build script

--- docs/make.jl
+++ docs/make.jl
@@ -8,5 +8,6 @@ makedocs(
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo   = "github.com/tawheeler/DC.jl.git"
+    repo   = "github.com/tawheeler/DC.jl.git",
+    julia  = "0.4"
 )
